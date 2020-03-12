e373b055b6e5f0fd224345f85effc4a843a2e9da
Luis Sandoval <luissandoval1126@gmail.com>
2020-03-11 23:11:07 -0700

    responsive prompting and looping questions

--- app.js
+++ app.js
@@ -1,35 +1,129 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+const Joi = require('Joi');
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Employee = require('./lib/Employee');
 const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+const employees =[];
+var res ='';
+
+function onValidation(err,val){
+    if(err){
+        console.log(err.message);
+        return err.message;         
+    }
+    else{
+        return true;            
+    }
+           
+};
+function validateName(name) {
+    return Joi.validate(name, Joi.string().required(), onValidation);
+};
+function validateEmail(email) {
+    return Joi.validate(email, Joi.string().email(), onValidation);
+};
+
+function promptInit(){  
+    return inquirer.prompt([
+        {
+        type: "input",
+        message: "What is the Employee's name?",
+        name: "name",
+        validate: validateName
+        },
+        {
+        type: "input",
+        message: "What is the Employee ID #?",
+        name: "id",
+        validate: (name)=> !isNaN(name)
+        },
+        {
+        type: "input",
+        message: "What is the employee's email?",
+        name: "email",
+        validate: validateEmail
+        },
+        {
+        type: "list",
+        message: "What is the employee's role?",
+        name: "role",
+        choices:[
+            "Manager",
+            "Engineer",
+            "Intern"
+            ]
+        },
+        {
+        type: "input",
+        message: "What is your office Number?",
+        name: "specific",
+        when:function( answers ) {
+            return answers.role === "Manager";
+          }
+        },
+        {
+        type: "input",
+        message: "What is your School's name?",
+        name: "specific",
+        when:function( answers ) {
+            return answers.role === "Intern";
+            }
+        },
+        {
+        type: "input",
+        message: "What is your GitHub Username?",
+        name: "specific",
+        when:function( answers ) {
+            return answers.role === "Engineer";
+            }
+        },
+        {
+            type: "confirm",
+            message: "Do you need to add another Employee?",
+            name: "another"
+            }
+    ])
+};
+function buildEmployee($answers) {
+    const  {name, id, email, role,specific, another} = $answers;
+    console.log($answers);
+    switch (role){
+        case 'Manager':
+           const man = new Manager(name, id, email, specific);
+            employees.push(man);
+        break;
+        case 'Engineer':
+            const engineer = new Engineer(name, id, email, specific);
+            employees.push(engineer);
+        break;
+        case 'Intern':
+            const intern = new intern(name, id, email, specific);
+            employees.push(intern);
+         break;
+        default:
+            const employee = new Employee(name, id, email);
+            employees.push(employee);
+        break;
+  };
+
+  if(another) {
+    buildAnother();  
+    } else {
+    console.log('All Done'); 
+    }
+};
+
+function buildAnother(){
+    promptInit()
+    .then(function(answer){
+        buildEmployee(answer);   
+    });
+};
+
+buildAnother();
